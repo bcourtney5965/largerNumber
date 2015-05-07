a71f84dbedbad8e231af413a7146c9058e5cd4b4
Brandon Courtney <brandon.h.courtney@gmail.com>
2015-05-07 12:38:01 -0500

    first & last commit for program

---
+++ biggerNumber.js
@@ -0,0 +1,9 @@
+function greaterNum (number1, number2) {
+	if (number1 === number2) {
+		console.log("The numbers" + " " + number1 + " " + "and" + " " + number2 + " " + "are equal." );
+	} else if (number1 > number2) {
+		console.log("Between the numbers" + " " + number1 + " " + "and" + " " + number2 + ", " + number1 + " " + "is the greater of the two.");
+	} else {
+		console.log("Between the numbers" + " " + number1 + " " + "and" + " " + number2 + ", " + number2 + " " + "is the greater of the two.");
+	}
+}
